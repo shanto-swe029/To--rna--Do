d307b9ef932b7e8b65e0ad2cb4f14bda0ca61c61
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-05-13 02:00:32 +0600

    CRUD operations complete

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -12,3 +12,17 @@ module.exports.saveToDo = async (req, res) => {
     res.send(data);
   });
 };
+
+module.exports.updateToDo = async (req, res) => {
+  const { _id, text } = req.body;
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Updated Successfully!"))
+    .catch((err) => console.log(err));
+};
+
+module.exports.deleteToDo = async (req, res) => {
+  const { _id } = req.body;
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Successfully!"))
+    .catch((err) => console.log(err));
+};
