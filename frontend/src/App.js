18ffd207caec10d58f69a2942d22500e9694514a
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-05-13 02:22:07 +0600

    frontend ongoing

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>TODO APP</h1>
+        <div className="top">
+          <input type="text" placeholder="type here" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
