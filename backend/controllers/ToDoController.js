25322f99387ecf99d4b1558f25df1ef0a4455f2f
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-05-13 00:59:36 +0600

    saveToDo controller added

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -4,3 +4,11 @@ module.exports.getToDo = async (req, res) => {
   const todo = await ToDoModel.find();
   res.send(todo);
 };
+
+module.exports.saveToDo = async (req, res) => {
+  const { text } = req.body;
+  ToDoModel.create({ text }).then((data) => {
+    console.log(`Added successfully: ${data}`);
+    res.send(data);
+  });
+};
--- backend/routes/ToDoRoutes.js
+++ backend/routes/ToDoRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
-const { getToDo } = require("../controllers/ToDoController");
+const { getToDo, saveToDo } = require("../controllers/ToDoController");
 
 const router = Router();
 
 router.get("/", getToDo);
+router.post("/save", saveToDo);
 
 module.exports = router;
