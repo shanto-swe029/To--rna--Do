b77d6f1e8d0d293062668f4a20d05b6a2a3ce7ba
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-05-13 00:38:10 +0600

    added ToDoRoutes

--- backend/Server.js
+++ backend/Server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes/ToDoRoutes");
 require("dotenv").config();
 
 const app = express();
 const PORT = process.env.port || 5000;
 
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
-
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log(`connected to mongoDB`))
   .catch((err) => console.log(err));
+
+app.use(routes);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
