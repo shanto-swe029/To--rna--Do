d562e667b8ada932451a9424f33acad9d0f108bf
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-05-13 00:52:36 +0600

    fresh start from routes

--- backend/controllers/ToDoController.js
+++ backend/controllers/ToDoController.js
@@ -1,5 +1,4 @@
 const ToDoModel = require("../models/ToDoModel");
-const todoModel = require("../models/ToDoModel");
 
 module.exports.getToDo = async (req, res) => {
   const todo = await ToDoModel.find();
--- backend/models/ToDoModel.js
+++ backend/models/ToDoModel.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const todoSchema = new mongoose.Schema({
-    text: {
-        type: String,
-        require: true 
-    }
-})
+  text: {
+    type: String,
+    require: true,
+  },
+});
 
-module.exports = mongoose.model('ToDo', todoSchema);
+module.exports = mongoose.model("ToDo", todoSchema);
