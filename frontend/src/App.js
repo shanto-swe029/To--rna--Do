b1368b1def1d45dd0be8291f8584056dab38f9b5
Ariful Islam Shanto <ariful.shanto786@gmail.com>
2024-05-13 23:47:51 +0600

    todo ui design complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div className="App">
@@ -5,6 +7,12 @@ function App() {
         <h1>TODO APP</h1>
         <div className="top">
           <input type="text" placeholder="type here" />
+          <div className="add">Add</div>
+        </div>
+        <div className="list">
+          <Todo text="buy shirt" />
+          <Todo text="pay bill"/>
+          <Todo text="shop from local store"/>
         </div>
       </div>
     </div>
